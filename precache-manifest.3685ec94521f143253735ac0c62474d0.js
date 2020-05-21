d24b667f9f0f129ceaf226d01da245b8779ecbf9
JoelZepeda <joe621.jayz@gmail.com>
2020-05-20 22:10:20 -0500

    Updates

---
+++ precache-manifest.3685ec94521f143253735ac0c62474d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d19f11113c6383d353e7",
+    "url": "/BlockChain_ETH-BLOCK/static/css/main.bbe2f1f1.chunk.css"
+  },
+  {
+    "revision": "d19f11113c6383d353e7",
+    "url": "/BlockChain_ETH-BLOCK/static/js/main.d19f1111.chunk.js"
+  },
+  {
+    "revision": "9555de888421bbf065ac",
+    "url": "/BlockChain_ETH-BLOCK/static/css/1.e0f9da36.chunk.css"
+  },
+  {
+    "revision": "9555de888421bbf065ac",
+    "url": "/BlockChain_ETH-BLOCK/static/js/1.9555de88.chunk.js"
+  },
+  {
+    "revision": "9b8c68410ae669446e64",
+    "url": "/BlockChain_ETH-BLOCK/static/js/runtime~main.9b8c6841.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/BlockChain_ETH-BLOCK/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "1b0f7f121a09a1321888da3c6361aa12",
+    "url": "/BlockChain_ETH-BLOCK/index.html"
+  }
+];
